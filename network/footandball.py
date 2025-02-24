85d4a23bf0b87a1da4c68d94a0837611a3438b1a
Serge Bishyr <seriybg@gmail.com>
2025-02-24 09:32:10 +0200

    change rnn arch

--- network/footandball.py
+++ network/footandball.py
@@ -344,7 +344,7 @@ def build_footandball_detector1(phase='train', max_player_detections=100, max_ba
     i_channels = 32
 
     base_net = fpn.FPN(layers, out_channels=out_channels, lateral_channels=lateral_channels, return_layers=[1, 3])
-    ball_classifier = CombinedClassifier(lateral_channels, hidden_dim=i_channels, output_dim=2)
+    ball_classifier = ClassifierRNN(lateral_channels, hidden_dim=i_channels, output_dim=2)
     # ball_classifier = nn.Sequential(nn.Conv2d(lateral_channels, out_channels=i_channels, kernel_size=3, padding=1),
     #                                 nn.ReLU(inplace=True),
     #                                 nn.Conv2d(i_channels, out_channels=2, kernel_size=3, padding=1))
