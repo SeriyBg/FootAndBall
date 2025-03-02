c55c1f712f0f5e092cbb216b6ea71c6b6c9d759c
Serge Bishyr <seriybg@gmail.com>
2025-03-02 19:30:33 +0200

    fix device

--- train_detector.py
+++ train_detector.py
@@ -133,8 +133,8 @@ def train(params: Params):
 
     # Create model
     device = "cuda" if torch.cuda.is_available() else 'cpu'
-    if torch.mps.device_count() > 0:
-        device = "mps"
+    # if torch.mps.device_count() > 0:
+    #     device = "mps"
 
     model = footandball.model_factory(params.model, 'train', weights_path=params.pretrained_weights)
     model.print_summary(show_architecture=True)
